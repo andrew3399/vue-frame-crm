3969c4e576f7ef090fccf5e60a8c0f77a504813a
lilycl <13521957015@163.com>
2018-05-29 11:37:14 +0800

    feat:修改报错，和端口

--- src/frame/frame.vue
+++ src/frame/frame.vue
@@ -4,13 +4,13 @@
         <div :class="['layout-sidebar',{'layout-sidebar--folded': isOpen === false},{'show': isOpenOnMinWin === false}]">
             <div class="layout-logo-left">
                 <slot name="frame-header">
-                    <span >
+                    <!-- <span v-if> -->
                         <!--<router-link :to="logoRouter">-->
-                            <img :src="img" alt="" class="layout-logo-img"/>
-                            <img :src="imgMin" alt="" class="layout-logo-min-img"/>
+                            <!-- <img :src="img" alt="" class="layout-logo-img"/> -->
+                            <!-- <img :src="imgMin" alt="" class="layout-logo-min-img"/> -->
                         <!--</router-link>-->
-                    </span>
-                    <span v-else>
+                    <!-- </span> -->
+                    <span >
                 <img :src="img" alt="" class="layout-logo-img"/>
                         <img :src="imgMin" alt="" class="layout-logo-min-img"/>
                     </span>
