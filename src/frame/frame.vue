cb7b1e2d2b5731abc4dc91ebf48ac801145fb7a3
lilycl <13521957015@163.com>
2018-06-28 10:37:18 +0800

    feat:修改面包屑

--- src/frame/frame.vue
+++ src/frame/frame.vue
@@ -258,7 +258,7 @@
                                     <t-breadcrumb-item v-for="(item, $idx) in breadcrumbArr" :key="$idx"
                                                        :class="($idx == 0 || $idx == breadcrumbArr.length - 1) ? 'unclick' : ''"
                                                        :href="($idx == 0 || $idx == breadcrumbArr.length - 1) ? '' : item.menuUrl">
-                                        {{lang === 'EN' ? item.menuDesc : item.menuName}}
+                                        {{lang === 'EN' ? item.menuEnName : item.menuName}}
                                     </t-breadcrumb-item>
                                 </t-breadcrumb>
                             </div>
