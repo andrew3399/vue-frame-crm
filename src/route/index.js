94efa11c44c5e7d9b39ac0a7b5eb2936d0feca72
lilycl <13521957015@163.com>
2018-09-14 11:33:28 +0800

    feat: 权限问题 1.1.17

--- src/route/index.js
+++ src/route/index.js
@@ -12,46 +12,73 @@ export function beforeEach (to, from, next, authorization, requestInstance, cb) 
   let accessToken = localStorage.get('access_token')
   let refreshToken = localStorage.get('refresh_token')
   let sessionTime = localStorage.get('session_time')
-    // 路由拦截 根据路由配置中meta.requireAuth判断是否需要登录
+  // 路由拦截 根据路由配置中meta.requireAuth判断是否需要登录
   let locationHref = window.location.href;
   locationHref = locationHref.replace(/[?]{0,}code=\w*[&]{0,}/g, '');
   locationHref = locationHref.replace(/state=\w*[&]{0,}/g, '');
-  if (to.meta.requireAuth) {
-    if (accessToken && refreshToken && sessionTime) {
-      //校验是否有菜单权限信息
-      let authorMenuArray = JSON.parse(sessionStorage.getItem('authorMenuArray'))
-      let routeArr = ['/res', '/cust', '/order', '/acct','/mks', '/rpt', '/prod', '/odp', '/base', '/']
-      if ((authorMenuArray != null && authorMenuArray.length > 0 && authorMenuArray.indexOf(to.path) > -1) || (routeArr.indexOf(to.path) > -1)  ){
+  //校验是否有菜单权限信息
+  let authorMenuArray = JSON.parse(sessionStorage.getItem('authorMenuArray'))
+  let routeArr = ['/res', '/cust', '/order', '/acct','/mks', '/rpt', '/prod', '/odp', '/base', '/']
+
+    if (to.meta.requireAuth) {
+      if (accessToken && refreshToken && sessionTime) {
         next()
       } else {
-          if (to.matched != null && to.matched.length > 0){
-            next({
-              path: to.matched[0].path
-            })
-          } else {
-            next({
-              path: authorization.redirect_uri
-            })
-          }
-      }
-    } else {
-      sessionStorage.clear()
-      let code = getQuery('code')
-      let state = getQuery('state')
-      if (code && state) {
-                /**
-                 *  这里需要去请求token的值,并设置sessiion-time
-                 *  oauth2
-                 */
-                // cb(code, state, next, localStorage, uuid(6, 16))
+        sessionStorage.clear()
+        let code = getQuery('code')
+        let state = getQuery('state')
+        if (code && state) {
+          /**
+           *  这里需要去请求token的值,并设置sessiion-time
+           *  oauth2
+           */
+          // cb(code, state, next, localStorage, uuid(6, 16))
 
-        requestInstance.post(authorization.tokenUri + '?code=' + code + '&state=' + state +
-                    '&grant_type=authorization_code' + '&client_id=' + authorization.client_id + '&redirect_uri=' + encodeURIComponent(locationHref))
-                    .then(res => {
-                      let time = new Date().getTime() + 8 * 60 * 60 * 1000
+          requestInstance.post(authorization.tokenUri + '?code=' + code + '&state=' + state +
+            '&grant_type=authorization_code' + '&client_id=' + authorization.client_id + '&redirect_uri=' + encodeURIComponent(locationHref))
+            .then(res => {
+              let time = new Date().getTime() + 8 * 60 * 60 * 1000
+              localStorage.set('access_token', res.data.access_token, res.data.expires_in * 1000)
+              localStorage.set('refresh_token', res.data.refresh_token, Math.pow(2, 32))
+              localStorage.set('session_time', time, 8 * 60 * 60 * 1000)
+              next()
+            }).catch(res => {
+            let msg = {
+              client_id: authorization.client_id,
+              redirect_uri: encodeURIComponent(locationHref),
+              state: uuid(6, 16)
+            }
+            window.location.href = authorization.authorizeUri + '?client_id=' + msg.client_id + '&redirect_uri=' + msg.redirect_uri + '&response_type=code&scope=read&state=' + msg.state
+            next()
+          })
+        } else {
+          /**
+           * 需要判断页面sessionTime是否生效，如果失效，需要重新去登录
+           */
+          if (sessionTime && !accessToken && refreshToken) {
+            /**
+             * 如果生效，且accessToken 失效，
+             * 需要重新获取accessToken
+             */
+            requestInstance.post(authorization.tokenUri + '?grant_type=refresh_token' + '&refresh_token=' + encodeURIComponent(refreshToken) + '&scope=read', '', {
+              headers: {
+                Authorization: 'Basic ' + Base64.encode(authorization.client_id + ':' + authorization.clientSecret)
+              }
+            }).then(res => {
+              localStorage.set('access_token', res.data.access_token, res.data.expires_in * 1000)
+              // localStorage.set('refresh_token', res.data.refresh_token, Math.pow(2, 32))
+              next()
+            }).catch(res => {
+              if (res && res.response) {
+                switch (res.response.status) {
+                  case 401:
+                    requestInstance.post(authorization.tokenUri + '?grant_type=refresh_token' + '&refresh_token=' + refreshToken + '&scope=read', '', {
+                      headers: {
+                        Authorization: 'Basic ' + Base64.encode(authorization.client_id + ':' + authorization.clientSecret)
+                      }
+                    }).then(res => {
                       localStorage.set('access_token', res.data.access_token, res.data.expires_in * 1000)
-                      localStorage.set('refresh_token', res.data.refresh_token, Math.pow(2, 32))
-                      localStorage.set('session_time', time, 8 * 60 * 60 * 1000)
+                      // localStorage.set('refresh_token', res.data.refresh_token, Math.pow(2, 32))
                       next()
                     }).catch(res => {
                       let msg = {
@@ -60,60 +87,22 @@ export function beforeEach (to, from, next, authorization, requestInstance, cb) 
                         state: uuid(6, 16)
                       }
                       window.location.href = authorization.authorizeUri + '?client_id=' + msg.client_id + '&redirect_uri=' + msg.redirect_uri + '&response_type=code&scope=read&state=' + msg.state
-                      next()
                     })
-      } else {
-                /**
-                 * 需要判断页面sessionTime是否生效，如果失效，需要重新去登录
-                 */
-        if (sessionTime && !accessToken && refreshToken) {
-                    /**
-                     * 如果生效，且accessToken 失效，
-                     * 需要重新获取accessToken
-                     */
-          requestInstance.post(authorization.tokenUri + '?grant_type=refresh_token' + '&refresh_token=' + encodeURIComponent(refreshToken) + '&scope=read', '', {
-            headers: {
-              Authorization: 'Basic ' + Base64.encode(authorization.client_id + ':' + authorization.clientSecret)
-            }
-          }).then(res => {
-            localStorage.set('access_token', res.data.access_token, res.data.expires_in * 1000)
-                        // localStorage.set('refresh_token', res.data.refresh_token, Math.pow(2, 32))
-            next()
-          }).catch(res => {
-            if (res && res.response) {
-              switch (res.response.status) {
-                case 401:
-                  requestInstance.post(authorization.tokenUri + '?grant_type=refresh_token' + '&refresh_token=' + refreshToken + '&scope=read', '', {
-                    headers: {
-                      Authorization: 'Basic ' + Base64.encode(authorization.client_id + ':' + authorization.clientSecret)
-                    }
-                  }).then(res => {
-                    localStorage.set('access_token', res.data.access_token, res.data.expires_in * 1000)
-                                        // localStorage.set('refresh_token', res.data.refresh_token, Math.pow(2, 32))
-                    next()
-                  }).catch(res => {
-                    let msg = {
-                      client_id: authorization.client_id,
-                      redirect_uri: encodeURIComponent(locationHref),
-                      state: uuid(6, 16)
-                    }
-                    window.location.href = authorization.authorizeUri + '?client_id=' + msg.client_id + '&redirect_uri=' + msg.redirect_uri + '&response_type=code&scope=read&state=' + msg.state
-                  })
-                  break
+                    break
+                }
               }
+            })
+          } else {
+            let msg = {
+              client_id: authorization.client_id,
+              redirect_uri: encodeURIComponent(locationHref),
+              state: uuid(6, 16)
             }
-          })
-        } else {
-          let msg = {
-            client_id: authorization.client_id,
-            redirect_uri: encodeURIComponent(locationHref),
-            state: uuid(6, 16)
+            window.location.href = authorization.authorizeUri + '?client_id=' + msg.client_id + '&redirect_uri=' + msg.redirect_uri + '&response_type=code&scope=read&state=' + msg.state
           }
-          window.location.href = authorization.authorizeUri + '?client_id=' + msg.client_id + '&redirect_uri=' + msg.redirect_uri + '&response_type=code&scope=read&state=' + msg.state
         }
       }
+    } else {
+      next()
     }
-  } else {
-    next()
-  }
 }
